3bfd6fdd9115dc48b45575bdcd3aba0240f3de76
Brent <bchyman@hotmail.com>
2020-02-09 18:55:42 -0700

    update

--- OHLC Data Analysis/Get OHLC Data.py
+++ OHLC Data Analysis/Get OHLC Data.py
@@ -4,15 +4,7 @@ import datetime
 import sys
 import os
 
-# have the user specify the amt of days to go back from the given date (this could be good for back testing)
-# can calcuate a datediff from date range
-
-# Will only have to work with a single data frame. Use a  
-# window function to calculate the price changes
-
-# Going to give the user the ability to specify an interval too
-
-# Prompt user for date range (make an interface for this? )
+# Prompt user (make an interface for this? )
 inputDt = input("date (mm/dd/YYYY): ") # should put in error handling here...
 inputHistoricalDayCount = input("historical day count: ")
 endDt = pd.Timestamp(inputDt, tz = 'US/Eastern').isoformat()
@@ -22,10 +14,12 @@ api = tradeapi.REST()
 
 # Get the list of all ticker symbols in alpaca
 #   -> Will need to make an api call for all active assets
-#   -> would like to make just a plain "get asset list" script
 assetFilter = ["AAPL","TSLA","F"]
 assets = api.list_assets(status="active") 
-tickerSymbols = [asset.symbol for asset in assets if asset.symbol in assetFilter]
+if len(assetFilter) > 0:
+    tickerSymbols = [asset.symbol for asset in assets if asset.symbol in assetFilter]
+else:
+    tickerSymbols = [asset.symbol for asset in assets]
 tickerSymbolCount = len(tickerSymbols)
 
 # Make api calls to get the data
@@ -45,7 +39,7 @@ for i in range(max_symbol_count_per_request , tickerSymbolCount + max_symbol_cou
     ]
 
     # api call for all barset data among all 100 current tickers in the ticker symbol subset
-    barset = api.get_barset(tickerSymbolSubSet, 'day', end=endDt, limit=inputHistoricalDayCount)
+    barset = api.get_barset(tickerSymbolSubSet, 'day', end=endDt, limit=int(inputHistoricalDayCount) + 1)
 
     # Create data frames for each ticker and append each of them into a list
     for tickerSymbol in tickerSymbolSubSet:
@@ -53,6 +47,7 @@ for i in range(max_symbol_count_per_request , tickerSymbolCount + max_symbol_cou
         # Generate data frame for current ticker and append ticker symbol to the ticker symbol col
         dFrame = barset[tickerSymbol].df
         dFrame["ticker symbol"] = tickerSymbol
+        dFrame['change'] = dFrame.close.diff(1)
 
         # append data frame to list, depending on the date
         dFrameList.append(dFrame)
@@ -63,26 +58,22 @@ for i in range(max_symbol_count_per_request , tickerSymbolCount + max_symbol_cou
 dFrame = pd.concat(dFrameList)
 
 # manipulate the dataframe a little
-#   -> drop the index and convert the datetimes into dates
+#   -> drop the index and 
+#   -> convert the datetimes into dates and 
+#      rename the time col to date
+#   -> reorder cols
 dFrame.reset_index(inplace=True)
 dFrame['time'] = dFrame['time'].apply(lambda x: x.date())
-# rename the time col, right here... re order the ticker symbol col
+dFrame = dFrame.rename(columns = {"time":"date"})
+dFrame = dFrame[['ticker symbol','date','open','high','low','close','change','volume']]
+
+# filter out records with NaN in the change
+dFrame = dFrame.dropna()
+
+# output the data from the data frame into a csv
+#   -> use the date in the name of the data file, create folder if it doesn't exist
+# create a text file to describe parameters used to generate data
+dFrame.to_csv(os.path.join("Data","stock prices.csv"), index = False)
 
 print(dFrame)
-
-# # extract min date from the pandas dataframe
-# print(dFrame["time"].min())
-# print(type(dFrame["time"].min()))
-
-# # THIS WILL ALL BE REPLACED BY THE WINDOW FUNCTION
-# outputDf = pd.merge(dFrame, dFrame[dFrame['time'] == datetime.date(2019, 12, 20)] ,on="ticker symbol")
-# outputDf = outputDf[outputDf["time_x"] == datetime.date(2019, 12, 23)]
-# outputDf['change'] = outputDf['close_y'] - outputDf['close_x']
-# outputDf = outputDf[["ticker symbol","close_x", "high_x", "change","time_x"]]
-
-# # output the data from the data frame into a csv
-# # use the date in the name of the data file, create folder if it doesn't exist
-# outputDf.to_csv(os.path.join("Data","stock prices.xlsx"), index = False)
-# # print(outputDf.memory_usage()) 
-
 print("Data file has been generated")

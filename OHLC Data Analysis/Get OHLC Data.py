48c2c911dd1ea4710cd1a6c3490f6bbcafb56432
Brent Hyman <bchyman@hotmail.com>
2020-02-09 17:19:03 -0700

    update - still a work in progress

--- OHLC Data Analysis/Get OHLC Data.py
+++ OHLC Data Analysis/Get OHLC Data.py
@@ -4,12 +4,13 @@ import datetime
 import sys
 import os
 
-# Will only have to work with a single data frame and use window functions to 
-# calculate the price changes changes
-#   -> user may be able to specify a time interval too
-
 # have the user specify the amt of days to go back from the given date (this could be good for back testing)
 # can calcuate a datediff from date range
+
+# Will only have to work with a single data frame. Use a  
+# window function to calculate the price changes
+
+# Going to give the user the ability to specify an interval too
 
 # Prompt user for date range (make an interface for this? )
 inputDt = input("date (mm/dd/YYYY): ") # should put in error handling here...
@@ -21,8 +22,10 @@ api = tradeapi.REST()
 
 # Get the list of all ticker symbols in alpaca
 #   -> Will need to make an api call for all active assets
-assets = api.list_assets(status="active") # can i apply the ticker symbol filter right here??
-tickerSymbols = [asset.symbol for asset in assets]
+#   -> would like to make just a plain "get asset list" script
+assetFilter = ["AAPL","TSLA","F"]
+assets = api.list_assets(status="active") 
+tickerSymbols = [asset.symbol for asset in assets if asset.symbol in assetFilter]
 tickerSymbolCount = len(tickerSymbols)
 
 # Make api calls to get the data
@@ -44,7 +47,7 @@ for i in range(max_symbol_count_per_request , tickerSymbolCount + max_symbol_cou
     # api call for all barset data among all 100 current tickers in the ticker symbol subset
     barset = api.get_barset(tickerSymbolSubSet, 'day', end=endDt, limit=inputHistoricalDayCount)
 
-    # Build/add to the data frame (add to a list instea)
+    # Create data frames for each ticker and append each of them into a list
     for tickerSymbol in tickerSymbolSubSet:
 
         # Generate data frame for current ticker and append ticker symbol to the ticker symbol col
@@ -52,36 +55,34 @@ for i in range(max_symbol_count_per_request , tickerSymbolCount + max_symbol_cou
         dFrame["ticker symbol"] = tickerSymbol
 
         # append data frame to list, depending on the date
-
-        if tickerSymbols.index(tickerSymbol) > 0:
-		
-            dFrame = pd.concat([lst_dFrame,dFrame])
-
-        lst_dFrame = dFrame
+        dFrameList.append(dFrame)
 
     indexStrt = i
 
-# build the data frame right here...
+# Concatenate all of the data frames into a single one
+dFrame = pd.concat(dFrameList)
 
 # manipulate the dataframe a little
 #   -> drop the index and convert the datetimes into dates
 dFrame.reset_index(inplace=True)
 dFrame['time'] = dFrame['time'].apply(lambda x: x.date())
-# rename the time col, right here...
+# rename the time col, right here... re order the ticker symbol col
 
-# extract min date from the pandas dataframe
-print(dFrame["time"].min())
-print(type(dFrame["time"].min()))
+print(dFrame)
 
-# will need to do this n times
-outputDf = pd.merge(dFrame, dFrame[dFrame['time'] == datetime.date(2019, 12, 20)] ,on="ticker symbol")
-outputDf = outputDf[outputDf["time_x"] == datetime.date(2019, 12, 23)]
-outputDf['change'] = outputDf['close_y'] - outputDf['close_x']
-outputDf = outputDf[["ticker symbol","close_x", "high_x", "change","time_x"]]
+# # extract min date from the pandas dataframe
+# print(dFrame["time"].min())
+# print(type(dFrame["time"].min()))
 
-# output the data from the data frame into a csv
-# use the date in the name of the data file, create folder if it doesn't exist
-outputDf.to_csv(os.path.join("Data","stock prices.xlsx"), index = False)
-# print(outputDf.memory_usage()) 
+# # THIS WILL ALL BE REPLACED BY THE WINDOW FUNCTION
+# outputDf = pd.merge(dFrame, dFrame[dFrame['time'] == datetime.date(2019, 12, 20)] ,on="ticker symbol")
+# outputDf = outputDf[outputDf["time_x"] == datetime.date(2019, 12, 23)]
+# outputDf['change'] = outputDf['close_y'] - outputDf['close_x']
+# outputDf = outputDf[["ticker symbol","close_x", "high_x", "change","time_x"]]
+
+# # output the data from the data frame into a csv
+# # use the date in the name of the data file, create folder if it doesn't exist
+# outputDf.to_csv(os.path.join("Data","stock prices.xlsx"), index = False)
+# # print(outputDf.memory_usage()) 
 
 print("Data file has been generated")
